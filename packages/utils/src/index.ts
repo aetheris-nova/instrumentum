f0d2c7c5b69cd85eebf98c49f74e3c1873b117de
Kieran O'Neill <hello@kieranoneill.com>
2025-01-18 10:52:57 +0000

    feat(utils): add islocalstorageavailable function

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,1 +1,4 @@
 // exports will be generated automatically generated using: npm run generate:index
+export { default as createLogger } from './createLogger';
+export { default as isLocalStorageAvailable } from './isLocalStorageAvailable';
+export { default as upsertItemsByKey } from './upsertItemsByKey';
