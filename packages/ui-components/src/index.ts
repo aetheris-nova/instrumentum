8ba41574a9c6d8a56559829fd94c8822bd0cdca2
Kieran O'Neill <hello@kieranoneill.com>
2025-01-18 12:53:49 +0000

    chore: squash

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -1,5 +1,5 @@
 // exports will be generated automatically generated using: npm run generate:index
-export { default as theme } from './theme';
+export { default as defaultTheme } from './theme';
 export * from './components';
 export * from './constants';
 export * from './hooks';
