0dca9a8b4be92ed22a59d108fb54e80ea3615467
Kieran O'Neill <hello@kieranoneill.com>
2025-01-30 09:37:32 +0000

    feat(utils): add distance measuring utils

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,11 @@
 // exports will be generated automatically generated using: npm run generate:index
+export { default as calculateDistanceBetweenPoints } from './calculateDistanceBetweenPoints';
 export { default as createLogger } from './createLogger';
+export { default as fetchSmartCharacterByAddress } from './fetchSmartCharacterByAddress';
 export { default as formatUnits } from './formatUnits';
 export { default as isLocalStorageAvailable } from './isLocalStorageAvailable';
+export { default as lightYearsToMeters } from './lightYearsToMeters';
+export { default as metersToLightYears } from './metersToLightYears';
+export { default as postMetaTransaction } from './postMetaTransaction';
 export { default as truncateText } from './truncateText';
 export { default as upsertItemsByKey } from './upsertItemsByKey';
