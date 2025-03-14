f1d55d6a64558f6e58664b5eebd1ef39c101bb89
Kieran O'Neill <hello@kieranoneill.com>
2025-03-14 17:17:51 +0000

    feat(utils): add ellipse text and is owner functions

--- packages/utils/src/types/index.ts
+++ packages/utils/src/types/index.ts
@@ -1,2 +1,3 @@
+export type { default as IEllipseTextOptions } from './IEllipseTextOptions';
 export type { default as IFormatUnitsOptions } from './IFormatUnitsOptions';
 export type { default as ITruncateTextOptions } from './ITruncateTextOptions';
